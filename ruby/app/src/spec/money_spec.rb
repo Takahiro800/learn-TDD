f0954fe7bc0765b004ab970b90e119d7e9c212f2
taemath <hirotaka081630@gmail.com>
2022-11-28 20:19:08 +0900

    test(ruby/p.14): dollarの副作用をどうする？

--- ruby/app/src/spec/money_spec.rb
+++ ruby/app/src/spec/money_spec.rb
@@ -7,9 +7,19 @@ require './dollar'
 # TODO Moneyの丸め処理どうする?
 
 RSpec.describe '仮実装' do
+  # FIXME: 必要なデータを切り出して準びする
   it '$5 * 2 = $10' do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq 10
+    # five.times(2)
+    # expect(five.amount).to eq 10
+
+    # five.times(3)
+    # expect(five.amount).to eq 15
+
+    product = five.times(2)
+    expect(product.amount).to eq 10
+
+    product = five.times(3)
+    expect(product.amount).to eq 15
   end
 end
