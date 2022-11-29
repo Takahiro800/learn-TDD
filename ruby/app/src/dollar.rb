905955cf931f1d14f3718f9560977ce0f46e4324
taemath <hirotaka081630@gmail.com>
2022-11-29 23:23:46 +0900

    refactor(ruby/p.37~38): moneykクラスへ一部を移動

--- ruby/app/src/dollar.rb
+++ ruby/app/src/dollar.rb
@@ -1,19 +1,8 @@
 require './money'
 
 class Dollar < Money
-  attr_reader :amount
-
-  def initialize(amount)
-    # CHECK superを使う場合は Effective Ruby(p.25~)参照する
-    @amount = amount
-  end
-
   def times(multiplier)
     Dollar.new(amount * multiplier)
-  end
-
-  def equals?(dollar)
-    amount == dollar.amount
   end
 
   # NOTE: Effective Ruby p.43
--- ruby/app/src/money.rb
+++ ruby/app/src/money.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class Money
+  attr_reader :amount
+
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def equals?(money)
+    amount == money.amount
+  end
 end
