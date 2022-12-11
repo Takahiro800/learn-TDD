d973127652aa2a750e9f7fee4f13c100918c7fb0
taemath <hirotaka081630@gmail.com>
2022-12-11 23:40:29 +0900

    feat(ruby/p.143~144): 第二部 makeコマンドの用意

---
+++ ruby/xunit/xunit.rb
@@ -0,0 +1,14 @@
+# TODO: テストメソッドを呼び出す
+# TODO set_upを最初に呼び出す
+# TODO tear_downを後で呼び出す
+# TODO テストメソッドが失敗したとしてもtear_downを呼び出す
+# TODO 複数のテストを呼び出す
+# TODO 収集したテスト結果を出力する
+
+class WasRun
+end
+
+test = WasRun.new('test_method')
+puts test.was_run
+test.test_method
+puts test.was_run
